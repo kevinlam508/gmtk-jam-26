95e47e04fcbaa0880ec4b194277d66bfd21e4d41
Kevin Lam <kevin.lam72003@gmail.com>
2026-07-25 17:24:52 -0500

    Level timer in UI and skybox

--- gmtk-gam-26/Assets/Scripts/UI/TimerCanvas.cs
+++ gmtk-gam-26/Assets/Scripts/UI/TimerCanvas.cs
@@ -1,9 +1,16 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public class TimerCanvas : MonoBehaviour
 {
+    [SerializeField] private TMP_Text _timerLabel;
+    [SerializeField] private string _timeFormat = "<mspace=0em>{0}</mspace>";
+
     public void UpdateGlobalTimerUI(float currentTime, float totalTime)
     {
-        
+        _timerLabel.text = string.Format(_timeFormat, 
+            TimeSpan.FromSeconds(currentTime).ToString("mm"),
+            TimeSpan.FromSeconds(currentTime).ToString("ss"));
     }
 }
