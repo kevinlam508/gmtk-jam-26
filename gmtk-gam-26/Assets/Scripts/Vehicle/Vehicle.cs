1909602d2c9422ee65c421f4c53c0f3cd24b8f03
Kevin Lam <kevin.lam72003@gmail.com>
2026-07-25 11:29:41 -0500

    Tire spin

--- gmtk-gam-26/Assets/Scripts/Vehicle/Vehicle.cs
+++ gmtk-gam-26/Assets/Scripts/Vehicle/Vehicle.cs
@@ -11,6 +11,10 @@ public class Vehicle : MonoBehaviour
     [SerializeField] private float _tireTurnAngle = 30f;
     [SerializeField] private Transform[] _frontTireTransforms;
     [SerializeField] private Transform[] _backTireTransforms;
+    [Min(.001f)]
+    [Tooltip("For determining spin speed from velocity. Bigger = slower spin")]
+    [SerializeField] private float _tireRadius;
+    [SerializeField] private Transform[] _tireVisuals;
 
     [Header("Movement")]
     [SerializeField] private float _defaultSpeed = 2f;
@@ -153,6 +157,16 @@ public class Vehicle : MonoBehaviour
             * _body.mass;
         _body.AddForce(sideDirection * appliedSideForce * groundedRatio);
         Debug.DrawRay(_body.position, sideDirection * 5 * Mathf.Sign(appliedSideForce), Color.blue, -1);
+
+        if (IsGrounded)
+        {
+            float tireAngularChange = forwardSpeed / _tireRadius * timeStep;
+            Quaternion tireRotation = Quaternion.Euler(tireAngularChange * Mathf.Rad2Deg, 0, 0);
+            foreach (Transform tireVisual in _tireVisuals)
+            {
+                tireVisual.localRotation *= tireRotation;
+            }
+        }
     }
 
     private void ProcessSteer(float timeStep, float groundedRatio)
