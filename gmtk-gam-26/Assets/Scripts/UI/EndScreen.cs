a97262d478e5c28325a935d923db91c13de2756b
Kevin Lam <kevin.lam72003@gmail.com>
2026-07-25 19:15:49 -0500

    Basic end screen

---
+++ gmtk-gam-26/Assets/Scripts/UI/EndScreen.cs
@@ -0,0 +1,26 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class EndScreen : MonoBehaviour
+{
+    public static bool Won;
+    public static int MoneyCollected;
+    public static int ContractsCaught;
+    public static float TimeLeft;
+
+    [SerializeField] private TMP_Text _wonLabel;
+    [SerializeField] private TMP_Text _moneyCollectedLabel;
+    [SerializeField] private TMP_Text _contractsCaughtLabel;
+    [SerializeField] private TMP_Text _timeLeftLabel;
+
+    void Start()
+    {
+        _wonLabel.text = Won ? "Victory" : "Failure";
+        _moneyCollectedLabel.text = $"Earned ${MoneyCollected:n0}";
+        _contractsCaughtLabel.text = $"Caught {ContractsCaught} suckers";
+
+        TimeSpan time = TimeSpan.FromSeconds(TimeLeft);
+        _timeLeftLabel.text = $"Time Left {time.ToString("mm\\:ss")}";
+    }
+}
