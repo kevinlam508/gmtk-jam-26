07438e6f23218b0a3e0a9de618a468b9f9318c32
hexxag0nn <58057979+hexxag0nn@users.noreply.github.com>
2026-07-26 01:40:34 -0500

    hud juice for completing or failing a contract

--- gmtk-gam-26/Assets/Scripts/UI/TimerCanvas.cs
+++ gmtk-gam-26/Assets/Scripts/UI/TimerCanvas.cs
@@ -1,16 +1,42 @@
 using System;
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
 public class TimerCanvas : MonoBehaviour
 {
     [SerializeField] private TMP_Text _timerLabel;
     [SerializeField] private string _timeFormat = "<mspace=0em>{0}</mspace>";
+    [SerializeField] private Color _warningColor;
+    [SerializeField] private float _punchScale;
+    private Vector3 _scaleVector;
+    [SerializeField] private float _shakeDistance;
+    private Vector3 _shakeVector;
+    private Tween shakeTween;
+    [SerializeField] private int vibrato;
+    
+    private void Start()
+    {
+        _scaleVector = Vector3.one * _punchScale;
+        shakeTween = _timerLabel.transform.DOShakePosition(1, _shakeVector, vibrato).SetLoops(-1);
+        shakeTween.Pause();
+    }
 
     public void UpdateGlobalTimerUI(float currentTime, float totalTime)
     {
         _timerLabel.text = string.Format(_timeFormat, 
             TimeSpan.FromSeconds(currentTime).ToString("mm"),
             TimeSpan.FromSeconds(currentTime).ToString("ss"));
+
+        if (currentTime <= 10)
+        {
+            _timerLabel.color = _warningColor;
+            _timerLabel.transform.DOPunchScale(_scaleVector, 0.1f);
+            if (currentTime <= 3)
+            {
+                shakeTween = _timerLabel.transform.DOShakePosition(1, _shakeVector, vibrato).SetLoops(-1);
+                shakeTween.Play();
+            }
+        }
     }
 }
